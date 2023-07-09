716ab728af4cbde8eb3057725d34ab6105a22c18
Sohag <ihsohag-84@gmail.com>
2023-07-09 14:24:04 +0600

    set app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:twitch_clone/screen/onboarding_screen.dart';
+import 'package:twitch_clone/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,16 +10,24 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Twitch Clone',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: AppBarTheme().copyWith(
+          centerTitle: true,
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: primaryColor,
+          ),
+          iconTheme: IconThemeData(color: primaryColor),
+        ),
       ),
       home: const OnBoardingScreen(),
     );
--- lib/screen/onboarding_screen.dart
+++ lib/screen/onboarding_screen.dart
@@ -6,7 +6,9 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Hello world"),
+      ),
     );
   }
 }
