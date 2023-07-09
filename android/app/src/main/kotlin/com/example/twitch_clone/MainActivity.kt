5a66430a26ae7e9ce4d9709c18cd862b5cfdfe20
Sohag <ihsohag-84@gmail.com>
2023-07-09 14:15:08 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/twitch_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twitch_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
