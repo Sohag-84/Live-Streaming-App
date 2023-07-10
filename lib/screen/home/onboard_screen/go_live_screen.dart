1d6d00015cc5749b5b04526c1d00e1e313d261b9
Sohag <ihsohag-84@gmail.com>
2023-07-10 11:11:53 +0600

    pick image from gallery

--- lib/screen/home/onboard_screen/go_live_screen.dart
+++ lib/screen/home/onboard_screen/go_live_screen.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:typed_data';
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:twitch_clone/utils/colors.dart';
+import 'package:twitch_clone/utils/utils.dart';
 import 'package:twitch_clone/widgets/custom_button.dart';
 import 'package:twitch_clone/widgets/custom_textfield.dart';
 
@@ -15,6 +18,7 @@ class GoLiveScreen extends StatefulWidget {
 
 class _GoLiveScreenState extends State<GoLiveScreen> {
   final _titleController = TextEditingController();
+  Uint8List? image;
 
   @override
   void dispose() {
@@ -35,40 +39,57 @@ class _GoLiveScreenState extends State<GoLiveScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(height: 20),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 22.0),
-                    child: DottedBorder(
-                      borderType: BorderType.RRect,
-                      radius: Radius.circular(10),
-                      dashPattern: const [10, 4],
-                      strokeCap: StrokeCap.round,
-                      color: buttonColor,
-                      child: Container(
-                        width: double.infinity,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          color: buttonColor.withOpacity(.05),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.folder_open,
-                              size: 40,
-                              color: buttonColor,
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              "Select your thumbnail",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.grey.shade400,
+                  InkWell(
+                    onTap: () async {
+                      Uint8List? pickedImage = await pickImage();
+                      if (pickedImage != null) {
+                        setState(() {
+                          image = pickedImage;
+                        });
+                      }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 22.0),
+                      child: image != null
+                          ? Center(
+                              child: SizedBox(
+                                height: 300,
+                                child: Image.memory(image!),
                               ),
                             )
-                          ],
-                        ),
-                      ),
+                          : DottedBorder(
+                              borderType: BorderType.RRect,
+                              radius: Radius.circular(10),
+                              dashPattern: const [10, 4],
+                              strokeCap: StrokeCap.round,
+                              color: buttonColor,
+                              child: Container(
+                                width: double.infinity,
+                                height: 150,
+                                decoration: BoxDecoration(
+                                  color: buttonColor.withOpacity(.05),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.folder_open,
+                                      size: 40,
+                                      color: buttonColor,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      "Select your thumbnail",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.grey.shade400,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
                     ),
                   ),
                   SizedBox(height: 15),
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,23 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar({required BuildContext context, required String content}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(content: Text(content)),
   );
+}
+
+Future<Uint8List?> pickImage() async {
+  FilePickerResult? pickedImage =
+      await FilePicker.platform.pickFiles(type: FileType.image);
+  if (pickedImage != null) {
+    if (kIsWeb) {
+      return pickedImage.files.single.bytes;
+    }
+    return await File(pickedImage.files.single.path!).readAsBytes();
+  }
+  return null;
 }
